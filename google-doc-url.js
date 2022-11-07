845aaa0d10efd9073b1663065223281b0b813059
BaronHarkonen <74378749+BaronHarkonen@users.noreply.github.com>
2022-11-07 15:27:32 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = ' https://baronharkonen.github.io/cokolwiek/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1lMMU46ek6HrMEZL7UQOel2K6y-2UCX0itu-Up5xDl2Y/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
